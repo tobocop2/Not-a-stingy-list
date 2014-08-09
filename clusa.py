f7aa6879fcf825254c955bf5fd23d028f3e4ee94
tobias <tobias.perelstein@gmail.com>
2014-08-09 02:50:20 -0400

    parsing search results

--- clusa.py
+++ clusa.py
@@ -1,11 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-def search_all_cities():
+
+#test query
+query = "9c1";
+
+def search_all_cities(query):
+
     r = requests.get("http://geo.craigslist.org/iso/us/")
     html_text = r.text
     soup = BeautifulSoup(html_text)
 
     for child in soup.find_all(id='list'):
-        for link in x.find_all('a'):
+        for link in child.find_all('a'):
+            # do some stuff with each link
             print(link.get('href'))
+            get_results(link.get('href'),query)
+
+    r.close
+
+def get_results(link):
+    r = requests.get(link+"/search/sss?query="+query+"&sort=rel")
+    html_text = r.text
+    soup = BeautifulSoup(html_text)
+    soup.find_all("div",class_="content")
+
+
 
