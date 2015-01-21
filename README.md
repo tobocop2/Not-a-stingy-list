a8864798989b4830ba6cdcfefd644101bdb4e16f
Tobias <tobias@perelste.in>
2015-01-21 00:12:52 +0000

    Add multi-page search and sorting by city/time

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-The start of a craigslist parser.
+NOT A STINGY LIST
+=================
+
+Here we have a script that I hope to ultimately use as a first step for providing an alternate viewing experience to the
+traditional craigslist search. Typically, you are only able to search for things on craigslist on a city by city basis.
+
+I personally have spent hours in the past searching every city for cars using tools such as Search Tempest, but now I am able to generate
+the the results for any query countrywide. I sort the result by city and by most recent time posted.
+
+A few years ago craiglook.com accomplished this task but was taken down due to violation of the terms of Craigslist. Since there
+currently aren't any tools that do exactly what I need, I periodically use this script to get the most recent results for the car
+that I have been searching for.
+
+TODO:
+1) Improve performance
+2) Add caching layer of some sort
+3) finish flask app or start over with Django.
---
+++ nasl.py
@@ -0,0 +1,134 @@
+import requests
+import grequests
+import random
+import time
+from user_agents import user_agents
+from bs4 import BeautifulSoup
+
+
+print "Enter your query here: \n"
+query = raw_input().replace(' ','+')
+
+#result paring
+result = {}
+#sorted by time
+result2 = {}
+
+#list for all request urls
+url_list = []
+
+#resulting output file
+f=open('result.log', 'w')
+f2=open('result1.log', 'w')
+
+def search_all_cities():
+
+    #random user agent taken from the list of user agents
+    rand_user_agent = user_agents[random.randint(0,len(user_agents)-1)]
+    user_agent = {'User-Agent': rand_user_agent}
+    req = requests.get("http://geo.craigslist.org/iso/us/",headers=user_agent)
+    html_text = req.text
+    soup = BeautifulSoup(html_text)
+
+    for child in soup.find_all(id='list'):
+        for link in child.find_all('a'):
+            search_link = link.get('href')+"/search/sss?query="+query+"&sort=rel"
+            res = requests.get(search_link)
+            soup = BeautifulSoup(res.text)
+            print soup.select('.totalcount')
+            if len(soup.select('.totalcount')) > 0:
+                count = int(soup.select('.totalcount')[0].text)
+                print 'The item count %d' % count
+                url_list.append(search_link)
+                for i in range(100,count,100):
+                    next_link = "%s/search/sss?s=%d&query=%s&sort=rel" % (link.get('href'),i,query)
+                    print search_link
+                    print next_link
+                    url_list.append(next_link)
+            else:
+                url_list.append(search_link)
+                print search_link
+
+    req.close
+
+#The div with the "content" class contains all of the results. Each "hdrlnk" class contains the
+#description as well as the url, which is why it's convenient to use this class.
+
+def get_results():
+
+    #sleep_time = random.random()
+    #time.sleep(sleep_time)
+    rand_user_agent = user_agents[random.randint(0,len(user_agents)-1)]
+    user_agent = {'User-Agent': rand_user_agent}
+
+
+    rs = (grequests.get(link,stream=False) for link in url_list)
+
+    responses = grequests.map(rs,size=10)
+
+    for req in responses:
+        link = req.url.split('search')[0]
+        print "searching: "+link+"\nfor "+query+"."
+        html_text = req.text
+        soup = BeautifulSoup(html_text)
+        print link
+        for child in soup.find_all("div",class_="content"):
+            for result_link in child.find_all("a",class_="hdrlnk"):
+                #If a link exists  must check if the result is local to prepend the full link
+                if "html" in result_link.get('href'):
+                    if "http" not in result_link.get('href'):
+                        full_link = link+result_link.get('href')
+                    else:
+                        full_link = result_link.get('href')
+
+                    link_desc = result_link.get_text()
+                    try:
+                       link_desc.decode('ascii')
+                    except UnicodeError:
+                       link_desc = link_desc.encode('utf-8',"replace")
+                    page_res = requests.get(full_link)
+                    page_soup = BeautifulSoup(page_res.text)
+                    post_time_text = page_soup.find('time').text
+                    post_time = str(post_time_text[:-2])
+                    time_of_day = str(post_time_text[-2:])
+                    post_time_struct = time.strptime(post_time, "%Y-%m-%d %H:%M")
+                    unix_post_time = time.mktime(post_time_struct)
+                    if not link_desc in result:
+                       result[link_desc] = full_link
+                       result2[link_desc] = {full_link: unix_post_time}
+                    #print result2
+                       #print_result()
+        req.close()
+
+#Will include price later
+#for result_link in child.find_all("a",class_="i"):
+#get text from this child
+
+def print_result():
+    sorted_result = sorted([pair[::-1] for pair in (result.items())])
+    sorted_result2 = sorted(result2.items(), key=lambda pair: pair[1].values(),reverse=True)
+
+    f.write("<!DOCTYPE_HTML>\n <html>\n<head>\n<title>Not a stingy List</title>\n</head>\n<body>\n")
+    for pair in sorted_result:
+        #f.write("<br>"+str(pair[1])+"<a href="""+str(pair[0])+"<br>")
+        desc = str(pair[1])
+        url = str(pair[0])
+        f.write("<br>\n%s\n<br>\n<a href=\"%s\">%s</a><br>" % (desc,url,url))
+        print"\n"+str(pair[1])+"\n"+str(pair[0])+"\n"
+    f.write("<br></body>\n</html>")
+
+    f2.write("<!DOCTYPE_HTML>\n <html>\n<head>\n<title>Not a stingy List</title>\n</head>\n<body>\n")
+    for pair in sorted_result2:
+        desc = pair[0]
+        post_time = pair[1][pair[1].keys()[0]]
+        post_time = time.ctime(post_time)
+        url = pair[1].keys()[0]
+        f2.write("<br>\n%s\n<br>\nPosted at: %s\n<br><a href=\"%s\">%s</a><br>" % (desc,post_time,url,url))
+        print"\n"+str(pair[1])+"\n"+str(pair[0])+"\n"
+    f2.write("<br></body>\n</html>")
+
+
+search_all_cities()
+get_results()
+print_result()
+f.close
